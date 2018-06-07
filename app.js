b0aa894ecd46296edb6128bf761f906989b6d0e1
Demo-app <wouter.veneberg@ovsoftware.com>
2018-06-07 13:30:03 +0200

    test

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ MongoClient.connect(getConnection(), function(err, client) {
   console.log("Database connection ready");
 
   // Initialize the app.
-  var server = app.listen(8080, function () {
+  var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
   });
